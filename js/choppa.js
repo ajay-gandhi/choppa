7bda65590639ac964afd3d6e2fd5ba4299906ce3
Ajay Gandhi <aag255@cornell.edu>
2016-12-04 16:09:53 -0500

    Add bomb interactions

--- js/choppa.js
+++ js/choppa.js
@@ -62,6 +62,27 @@ Choppa.prototype.animate_mvmt = function (should) {
  */
 Choppa.prototype.drop = function () {
   var bomb = $('<div class="bomb"></div>');
+  bomb.css({
+    top: this.choppa_el.position().top,
+    left: this.choppa_el.position().left
+  });
+  console.log(bomb.css('top'), bomb.css('left'));
+  this.choppa_el.parent().append(bomb);
+
+  // Animate movement
+  bomb.animate({
+    top: '+=400',
+    left: '+=20'
+  }, {
+    duration: 6000,
+    specialEasing: {
+      top: 'linear',
+      left: 'bomb'
+    },
+    complete: function () {
+      bomb.remove();
+    }
+  });
 }
 
 /**
@@ -80,9 +101,10 @@ Choppa.prototype.bind = function () {
     return console.error('Could not start: choppa not rendered.');
 
   var self = this;
-  // Detect spacebar presses
-  $('body').keyup(function (e) {
-    if (e.keyCode == 32 && !is_reloading) {
+
+  // Function for interactions
+  var act = function () {
+    if (!self.is_reloading) {
       self.drop();
       self.is_reloading = true;
 
@@ -91,5 +113,22 @@ Choppa.prototype.bind = function () {
         me.is_reloading = false;
       }, 2000, self);
     }
+  };
+
+  // Detect spacebar presses and clicks
+  $('body').keyup(function (e) {
+    if (e.keyCode == 32) {
+      act();
+    }
   });
+  $(document).click(act);
 }
+
+/**
+ * Create a new easing function for bombs
+ */
+$.extend(jQuery.easing, {
+  bomb: function (x, t, b, c, d) {
+    return Math.pow(x, 0.25);
+  }
+});
